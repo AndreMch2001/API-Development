f69ace0722ae83cfa77a424a716fcb78c9de7271
Tillmann.J <139597953+JonasTillmann-J@users.noreply.github.com>
2026-03-05 22:56:05 +0000

    Correção Controller sistema subindo sem problema falta testar a base de dados

--- src/main/java/com/projeto/bolsafamilia/controller/Bolsafamiliacontroller.java
+++ src/main/java/com/projeto/bolsafamilia/controller/Bolsafamiliacontroller.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projeto.bolsafamilia.model.Bolsafamiliamodel;
-//import com.projeto.bolsafamilia.repository.BolsafamiliaRepository;
+import com.projeto.bolsafamilia.repository.BolsafamiliaRepository;
 import com.projeto.bolsafamilia.service.BolsafamiliaService;
 
 //import jakarta.persistence.criteria.Predicate;
@@ -30,14 +30,15 @@ import com.projeto.bolsafamilia.service.BolsafamiliaService;
 @CrossOrigin(origins = "*")
 public class Bolsafamiliacontroller {
 
-    //@Autowired
-    //private BolsafamiliaRepository repository;
+    @Autowired
+    private BolsafamiliaRepository repository;
+
     @Autowired
     private BolsafamiliaService service;
     //busca os dados do banco de dados, e retorna uma página de resultados, com base nos parâmetros de busca e paginação fornecidos na URL
     //
    @GetMapping("/busca")
-public Page<Bolsafamiliamodel> busca(
+    public Page<Bolsafamiliamodel> busca(
         @RequestParam(required = false) Long id,
         @RequestParam(required = false) String nome,
         @RequestParam(required = false) String uf,
@@ -47,7 +48,7 @@ public Page<Bolsafamiliamodel> busca(
         @RequestParam(required = false) BigDecimal valorMinimo,
         @RequestParam(required = false) BigDecimal valorMaximo,
         @RequestParam(defaultValue = "0") int pagina,
-        @RequestParam(defaultValue = "10") int tamanho) {
+        @RequestParam(defaultValue = "20") int tamanho) {
 
     return service.Busca(
             id,
@@ -62,11 +63,11 @@ public Page<Bolsafamiliamodel> busca(
             tamanho
     );
 }
-    
-    //encontra todos os dados do banco de dados, e retorna uma página de resultados, com base nos parâmetros de paginação fornecidos na URL
-    //@GetMapping("/todos")
-    
-    // Criamos a paginação explicitando que queremos ordenar pelo campo "id" de forma ASCENDENTE (do menor para o maior)
-    // Pageable paginacao = PageRequest.of(pagina, tamanho, Sort.by("id").ascending());return repository.findAll(paginacao);
+    @GetMapping("/todos")
+    public Page<Bolsafamiliamodel> listartodos(
+        @RequestParam(required = false) int pagina,
+        @RequestParam(required = false) int tamanho) {
+        return service.listarTodos(pagina, tamanho);
+        }
 }
 
