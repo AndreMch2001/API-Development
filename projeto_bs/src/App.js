bc3408547d710106038969c0bb32353caaee3e82
Tillmann.J <139597953+JonasTillmann-J@users.noreply.github.com>
2026-03-29 23:08:58 -0300

    Contrução HomePage - 29032026

--- projeto_bs/src/App.js
+++ projeto_bs/src/App.js
@@ -1,38 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
-
-function BtnTest(){ /*Função para Mostrar o Botão na tela*/
-  function FuncBtnTest(){ /*caso de tudo certo*/
-    try {/*Try caso tudo de certo*/
+/*function BtnTest(){ /*Função para Mostrar o Botão na tela
+  function FuncBtnTest(){ /*caso de tudo certo
+    try {/*Try caso tudo de certo
       return(
-         confirm("Deu Certo"),
-         console.log("FuncBtnTest Try - 200")
-      );
-    } catch (error) { /*Catch caso algo de errado*/
+         window.confirm("Tem certeza?"),
+         console.log("FuncBtnTest - Try - 200")
+      )
+    } catch (error) { /*Catch caso algo de errado
       return(
-        alert("Deu Certo Motivo : "),
-        console.log("FuncBtnTest Erro:" + error)
+        window.alert("Deu Certo Motivo : "),
+        console.log("FuncBtnTest - catch - Erro:" + error)
         
       );
-    } finally { /*caso de certo ou errado ele vai executar isso*/
+    } finally { /*caso de certo ou errado ele vai executar isso
       return(
-        console.log("FuncBtnTest Finally - 200")
+        console.log("FuncBtnTest - Finally - 200")
       );  
     }
   }
+    function btnOptLateral(){
+  
+}
+ 
   return(
     <button onClick={FuncBtnTest}>Clique em Mim Para testar o Sistema</button>
   );
 }
 
 /*--------------------------------------------------------------------------*/
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <BtnTest />
-      </header>
-    </div>
+    <main class="App-header">
+
+      <section class="App">
+
+        <section class="painelSuperior" id="painelSuperior">
+          <div class="elementoHamburger" id="elementoHamburger">
+            <img class="imgElemento-hamburger"src="/hamburger.png" alt="Menu" />
+          </div>
+          <ul class="listaNO" id="listaNO"> 
+            <li class="elementoListaNO-Entrar">
+              <a class="linkElemento-Entrar" href="https://www.google.com.br/" target="_blank" rel="noopener noreferrer">Entrar</a>
+            </li>
+            <li class="elementoListaNO-Cadastrar">
+              <a class="linkElemento-Cadastrar" href="https://www.google.com.br/" target="_blank" rel="noopener noreferrer">Cadastrar</a>
+            </li>
+          </ul>
+        </section>
+
+        <h1 class="txt_pg_principal">Seja Bem-vindo(a)</h1> 
+
+        <section class="painelLateral" id="painelLateral">
+        </section>
+      </section>
+    </main>
   );
 }
 
